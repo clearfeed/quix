234080882e88754d694ad2205a7deaac20ace491
imkhateeb <luckykhateeb2@gmail.com>
2025-03-24 22:22:18 +0530

    feat(database, slack): Refactor database module and enhance App Home view

--- src/slack/views/app_home.ts
+++ src/slack/views/app_home.ts
@@ -1,10 +1,15 @@
 import { HomeView } from "@slack/web-api";
 import { SLACK_ACTIONS } from "@quix/lib/utils/slack-constants";
-import { HomeViewArgs } from "./types";
-import { INTEGRATIONS } from "@quix/lib/constants";
+import { HomeViewArgs, PostgresConnectionModalArgs } from "./types";
+import { INTEGRATIONS, SUPPORTED_INTEGRATIONS } from "@quix/lib/constants";
 import { getInstallUrl } from "@quix/lib/utils/slack";
 import { HubspotConfig, JiraConfig, PostgresConfig, SlackWorkspace, GithubConfig, SalesforceConfig } from "@quix/database/models";
-import { BlockCollection, Elements, Bits, Blocks, Md, BlockBuilder } from "slack-block-builder";
+import { BlockCollection, Input, Section, Surfaces, Elements, Bits, Blocks, Md, BlockBuilder } from "slack-block-builder";
+import { WebClient } from "@slack/web-api";
+import { createHubspotToolsExport } from "agent-packages/packages/hubspot/dist";
+import { createJiraToolsExport } from "@clearfeed-ai/quix-jira-agent";
+import { createGitHubToolsExport } from '@clearfeed-ai/quix-github-agent';
+import { createPostgresToolsExport } from '@clearfeed-ai/quix-postgres-agent';
 
 export const getHomeView = (args: HomeViewArgs): HomeView => {
   const { selectedTool, slackWorkspace, connection } = args;
@@ -30,71 +35,39 @@ export const getHomeView = (args: HomeViewArgs): HomeView => {
   blocks.push(Blocks.Divider());
 
   if (selectedTool) {
-    const tool = INTEGRATIONS.find(integration => integration.value === selectedTool);
-    if (tool) {
-      const connectionInfo = getConnectionInfo(connection);
-      const capabilitiesMap: Record<string, { metadata: string[], examples: string[] }> = {
-        github: {
-          metadata: [
-            `👤 *Username:* ${(connection as GithubConfig).username}`,
-            `🔧 *Scopes:* ${(connection as GithubConfig).scopes?.join(', ') || 'N/A'}`
-          ],
-          examples: [
-            'Create an issue in repo `my-repo`',
-            'List all open PRs assigned to me',
-            'Get details of issue `#123`'
-          ]
-        },
-        jira: {
-          metadata: [
-            `🌐 *Jira URL:* ${(connection as JiraConfig).url}`,
-            `📁 *Project Key:* ${(connection as JiraConfig).default_config?.projectKey || 'Not set'}`
-          ],
-          examples: [
-            'Search for all open issues assigned to me',
-            'Get details for issue `JIRA-101`',
-            'List issues in project `ABC`'
-          ]
-        },
-        hubspot: {
-          metadata: [
-            `🌐 *Hub Domain:* ${(connection as HubspotConfig).hub_domain}`,
-            `🔐 *Scopes:* ${(connection as HubspotConfig).scopes?.join(', ') || 'N/A'}`
-          ],
-          examples: [
-            'Find contact with email `john@doe.com`',
-            'Get details of deal with ID `12345`',
-            'List all recently modified companies'
-          ]
-        },
-        postgres: {
-          metadata: [
-            `🛠️ *Host:* ${(connection as PostgresConfig).host}`,
-            `🗄️ *Database:* ${(connection as PostgresConfig).database}`,
-            `🔐 *User:* ${(connection as PostgresConfig).user}`,
-            `🔒 *SSL Enabled:* ${(connection as PostgresConfig).ssl ? 'Yes' : 'No'}`
-          ],
-          examples: [
-            'Get the top 5 customers by revenue',
-            'Show orders placed in the last 7 days',
-            'List employees in the Sales department'
-          ]
-        }
-      };
 
-      const toolData = capabilitiesMap[selectedTool];
-      if (toolData) {
-        blocks.push(
-          Blocks.Section({ text: `*You're connected to ${tool.name}!*` }),
-          Blocks.Context().elements(toolData.metadata),
-          Blocks.Section({
-            text: `*Here are some things you can ask Quix:*`
-          }),
-          Blocks.Section({
-            text: toolData.examples.map(e => `• *${e}*`).join('\n')
-          })
-        );
-      }
+    const toolData = getToolData({
+      slackWorkspace,
+      connection,
+      selectedTool,
+      userId: args.userId,
+    });
+
+    if (toolData.tool && toolData.configData) {
+      blocks.push(
+        Blocks.Section({
+          text: `🔗 *${toolData.tool.name} Connection Details:*`
+        }),
+        Blocks.Context().elements(toolData.configData),
+        Blocks.Section({ text: '\n\n\n' })
+      );
+    }
+
+    if (toolData.availableFns) {
+      blocks.push(
+        Blocks.Section({
+          text: `💡 *Available Functions:*\n\n${toolData.availableFns?.join('\n\n')}`
+        }),
+        Blocks.Section({ text: '\n\n\n' })
+      );
+    }
+
+    if (toolData.capabilities) {
+      blocks.push(
+        Blocks.Section({
+          text: `🚀 *What you can ask Quix:*\n\n${toolData.capabilities?.map(item => `• ${item}`).join('\n\n')}`
+        })
+      );
     }
   } else {
     blocks.push(
@@ -113,7 +86,7 @@ export const getHomeView = (args: HomeViewArgs): HomeView => {
           `• *HubSpot:* Retrieve create hubspot deals, contacts effortlessly.`,
           '',
           '',
-          '*💡 Coming Soon:* Zendesk and more.',
+          '*💡 Coming Soon:* Zendesk, Salesforce and more.',
           '',
           '',
           '*🚀 Capabilities:*',
@@ -131,6 +104,171 @@ export const getHomeView = (args: HomeViewArgs): HomeView => {
   return {
     type: 'home',
     blocks: BlockCollection(blocks)
+  }
+}
+
+const getToolData = (args: HomeViewArgs) => {
+  const { selectedTool, slackWorkspace, connection } = args;
+  let tool, configData, availableFns, capabilities;
+  if (selectedTool) {
+    tool = INTEGRATIONS.find(integration => integration.value === selectedTool);
+  }
+  if (connection) {
+    configData = getToolConfigData(connection);
+  }
+  if (selectedTool && slackWorkspace) {
+    availableFns = getAvailableFns(selectedTool, slackWorkspace);
+  }
+  if (selectedTool) {
+    capabilities = getCapabilities(selectedTool);
+  }
+
+  return {
+    tool,
+    configData,
+    availableFns,
+    capabilities
+  }
+}
+
+const getToolConfigData = (connection: GithubConfig | JiraConfig | HubspotConfig | PostgresConfig) => {
+  switch (true) {
+  case connection instanceof JiraConfig:
+    return [
+      `👤 *Username:* ${connection.url}`
+    ];
+  case connection instanceof HubspotConfig:
+    return [
+      `🌐 *Hub Domain:* ${connection.hub_domain}`,
+    ]
+  case connection instanceof PostgresConfig:
+    return [
+      `🛠️ *Host:* ${connection.host}`,
+      `🗄️ *Database:* ${connection.database}`,
+      `🔐 *User:* ${connection.user}`,
+      `🔒 *SSL Enabled:* ${connection.ssl ? 'Yes' : 'No'}`
+    ]
+  case connection instanceof GithubConfig:
+    return [
+      `👤 *Username:* ${connection.username}`
+    ];
+  default:
+    return [
+      'No config data.'
+    ];
+  }
+}
+
+const getAvailableFns = (
+  selectedTool: typeof INTEGRATIONS[number]["value"],
+  slackWorkspace: SlackWorkspace
+) => {
+
+  const jiraConfig = slackWorkspace.jiraConfig;
+  const githubConfig = slackWorkspace.githubConfig;
+  const hubspotConfig = slackWorkspace.hubspotConfig;
+  const postgresConfig = slackWorkspace.postgresConfig;
+
+  if (selectedTool === SUPPORTED_INTEGRATIONS.JIRA) {
+    const tools = createJiraToolsExport({
+      host: 'test-url',
+      auth: { bearerToken: 'test-token' },
+    }).tools;
+
+    return tools.map((tool) => (
+      '• `' + tool.lc_kwargs.name + '`: ' + tool.lc_kwargs.description
+    ));
+  }
+
+  if (selectedTool === SUPPORTED_INTEGRATIONS.GITHUB) {
+    const tools = createGitHubToolsExport({
+      token: 'test-access-token',
+      owner: 'test-github-owner',
+      repo: 'test-github-repo',
+    }).tools;
+
+    return tools.map((tool) => (
+      '• `' + tool.lc_kwargs.name + '`: ' + tool.lc_kwargs.description
+    ));
+  }
+
+  if (selectedTool === SUPPORTED_INTEGRATIONS.HUBSPOT && hubspotConfig) {
+    const tools = createHubspotToolsExport({
+      accessToken: 'test-access-token',
+    }).tools;
+
+    return tools.map((tool) => (
+      '• `' + tool.lc_kwargs.name + '`: ' + tool.lc_kwargs.description
+    ));
+  }
+
+  if (selectedTool === SUPPORTED_INTEGRATIONS.POSTGRES && postgresConfig) {
+    const tools = createPostgresToolsExport({
+      host: 'test-host',
+      port: 8080,
+      user: 'test-user',
+      password: 'test-password',
+      database: 'test-db',
+      ssl: false,
+    }).tools;
+
+    return tools.map((tool) => (
+      '• `' + tool.lc_kwargs.name + '`: ' + tool.lc_kwargs.description
+    ));
+  }
+
+  return ['No Available functions.'];
+};
+
+const getCapabilities = (selectedTool: typeof INTEGRATIONS[number]["value"]) => {
+  switch (selectedTool) {
+  case SUPPORTED_INTEGRATIONS.JIRA:
+    return [
+      "Find all Jira issues mentioning payment failure",
+      "What’s the status of `PROJ-256`?",
+      "Create a bug in the `ABC` project titled 'Login button not responsive', assign it to `john.doe`, with high priority.",
+      "Assign issue `PROJ-142` to `alice.smith`",
+      "Add a comment to `PROJ-123`: 'Waiting for design team’s input.",
+      "Show me all comments on `PROJ-987`",
+      "Update `PROJ-321`: change the summary to 'Onboarding flow issues', assign it to user ID `abc123`, and set priority to Medium.",
+      "Search for users named `Rahul`"
+    ];
+  case SUPPORTED_INTEGRATIONS.HUBSPOT:
+    return [
+      "Find deals related to 'Website Redesign'",
+      "Search HubSpot deals that mention 'Q2 renewal'",
+      "Add a note to deal `934756`: 'Client approved the new pricing structure.'",
+      "Attach a note saying 'Follow up next Tuesday' to deal ID `872390`",
+      "Create a deal named 'Enterprise Website Project' worth $15,000 in negotiation stage",
+      "Create a contact for `John Doe`, `john.doe@example.com`, phone: `+1234567890`"
+    ]
+  case SUPPORTED_INTEGRATIONS.POSTGRES:
+    return [
+      "What tables do we have in our database?",
+      "What’s the schema of the `users` table?",
+      "Show columns and data types of the `orders` table",
+      "Give me the top 5 customers by `revenue`",
+      "Show all `employees` from the `Sales department`",
+      "Get `orders` placed in the last 7 days"
+    ]
+  case SUPPORTED_INTEGRATIONS.GITHUB:
+    return [
+      "Find all issues in the `backend` repo related to authentication bugs created by `johnsmith`",
+      "Show me the details of issue number 72 in the `frontend/org-xyz` repo",
+      "Assign issue #101 in website repo to user `alicehub`",
+      "Unassign bobdev from issue #204 in `api-server` repo of `org-xyz`",
+      "List all users in our GitHub org `openai-labs`",
+      "Create an issue in the `react/facebook` repo titled 'Crash on launch' with description 'The app crashes immediately after opening on iOS 17.'",
+      "Search for the keyword `useEffect` in the `dashboard-ui/imkhateeb` repository"
+    ];
+  default:
+    return [
+      "Ask questions like:",
+      "Give first 10 rows of `accounts` table.",
+      "Create a GitHub issue titled Bug in Login flow in `xyz/pqr` repository.",
+      "Create a deal named Website Upgrade worth $10,000 in stage negotiations.",
+      "Assign jira issue PROJ-123 to xyz."
+    ];
   }
 }
 
@@ -183,18 +321,18 @@ const getOpenAIView = (slackWorkspace: SlackWorkspace): BlockBuilder[] => {
 const getConnectionInfo = (connection: HomeViewArgs['connection']): string => {
   if (!connection) return '';
   switch (true) {
-    case connection instanceof JiraConfig:
-      return `Connected to ${connection.url}`;
-    case connection instanceof HubspotConfig:
-      return `Connected to ${connection.hub_domain}`;
-    case connection instanceof PostgresConfig:
-      return `Connected to ${connection.host}`;
-    case connection instanceof GithubConfig:
-      return `Connected to ${connection.username}`
-    case connection instanceof SalesforceConfig:
-      return `Connected to ${connection.instance_url}`
-    default:
-      return '';
+  case connection instanceof JiraConfig:
+    return `Connected to ${connection.url}`;
+  case connection instanceof HubspotConfig:
+    return `Connected to ${connection.hub_domain}`;
+  case connection instanceof PostgresConfig:
+    return `Connected to ${connection.host}`;
+  case connection instanceof GithubConfig:
+    return `Connected to ${connection.username}`
+  case connection instanceof SalesforceConfig:
+    return `Connected to ${connection.instance_url}`
+  default:
+    return '';
   }
 }
 
