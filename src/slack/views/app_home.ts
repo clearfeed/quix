84891bb420fd2a907e2dc32a9185a606384fc703
Md Khateebur Rab <49562163+imkhateeb@users.noreply.github.com>
2025-03-26 20:42:18 +0530

    [FEAT]: 39 show list of supported functions against an integration (#52)

--- src/slack/views/app_home.ts
+++ src/slack/views/app_home.ts
@@ -1,10 +1,16 @@
 import { HomeView } from "@slack/web-api";
 import { SLACK_ACTIONS } from "@quix/lib/utils/slack-constants";
 import { HomeViewArgs } from "./types";
-import { INTEGRATIONS } from "@quix/lib/constants";
+import { INTEGRATIONS, SUPPORTED_INTEGRATIONS } from "@quix/lib/constants";
 import { getInstallUrl } from "@quix/lib/utils/slack";
 import { HubspotConfig, JiraConfig, PostgresConfig, SlackWorkspace, GithubConfig, SalesforceConfig } from "@quix/database/models";
 import { BlockCollection, Elements, Bits, Blocks, Md, BlockBuilder } from "slack-block-builder";
+import { createHubspotToolsExport } from "agent-packages/packages/hubspot/dist";
+import { createJiraToolsExport } from "@clearfeed-ai/quix-jira-agent";
+import { createGitHubToolsExport } from '@clearfeed-ai/quix-github-agent';
+import { createPostgresToolsExport } from '@clearfeed-ai/quix-postgres-agent';
+import { createSalesforceToolsExport } from '@clearfeed-ai/quix-salesforce-agent';
+import { getToolConfigData } from "@quix/lib/utils/slack-home";
 
 export const getHomeView = (args: HomeViewArgs): HomeView => {
   const { selectedTool, slackWorkspace, connection } = args;
@@ -27,11 +33,168 @@ export const getHomeView = (args: HomeViewArgs): HomeView => {
   } else {
     blocks.push(...getNonAdminView(slackWorkspace));
   }
+
+  blocks.push(Blocks.Divider());
+
+  if (selectedTool) {
+
+    const toolData = getToolData({
+      slackWorkspace,
+      connection,
+      selectedTool,
+      userId: args.userId,
+    });
+
+    if (toolData.tool && toolData.configData) {
+      blocks.push(
+        Blocks.Section({
+          text: `🔗 *${toolData.tool.name} Connection Details:*`
+        }),
+        Blocks.Context().elements(toolData.configData),
+        Blocks.Section({ text: '\n\n\n' })
+      );
+    }
+
+    if (toolData.availableFns) {
+      blocks.push(
+        Blocks.Section({
+          text: `💡 *Available Functions:*\n\n${toolData.availableFns?.join('\n\n')}`
+        }),
+        Blocks.Section({ text: '\n\n\n' })
+      );
+    }
+
+    if (toolData.tool) {
+      blocks.push(
+        Blocks.Section({
+          text: `🚀 *What you can ask Quix:*\n\n${toolData.tool.capabilities?.map(item => `• ${item}`).join('\n\n')}`
+        })
+      );
+    }
+  } else {
+    blocks.push(
+      Blocks.Section({
+        text: '*Welcome to Quix Integrations!*'
+      }),
+      Blocks.Context().elements(
+        'Quix allows you to talk to your tools in plain English right from Slack. Connect any of your business tools to get started.'
+      ),
+      Blocks.Section({
+        text: [
+          '*🔗 Supported Integrations:*',
+          `• *PostgreSQL:* Query and interact with your postgres database.`,
+          `• *GitHub:* Search code, Create issues, fetch PRs, assign teammates.`,
+          `• *Jira:* Search, view, and manage jira issues easily.`,
+          `• *HubSpot:* Retrieve create hubspot deals, contacts effortlessly.`,
+          '',
+          '',
+          '*💡 Coming Soon:* Zendesk, Salesforce and more.',
+          '',
+          '',
+          '*🚀 Capabilities:*',
+          `• Ask questions like:`,
+          "• “Give first 10 rows of `accounts` table.”",
+          "• “Create a GitHub issue titled Bug in Login flow in `xyz/pqr` repository.”",
+          "• “Create a deal named Website Upgrade worth $10,000 in stage negotiations.”",
+          "• “Assign jira issue `PROJ-123` to `xyz`.”",
+          "• “Attach a note titled 'Call Summary' to opportunity `0065g00000XyZt2`: 'Call went well, decision expected by next week.”",
+        ].join('\n')
+      }),
+      Blocks.Context().elements('Connect a tool from the dropdown above to get started.')
+    );
+  }
+
   return {
     type: 'home',
     blocks: BlockCollection(blocks)
   }
 }
+
+const getToolData = (args: HomeViewArgs) => {
+  const { selectedTool, connection } = args;
+  let tool, configData, availableFns;
+  if (selectedTool) {
+    tool = INTEGRATIONS.find(integration => integration.value === selectedTool);
+  }
+  if (connection) {
+    configData = getToolConfigData(connection);
+  }
+  if (selectedTool) {
+    availableFns = getAvailableFns(selectedTool);
+  }
+
+  return {
+    tool,
+    configData,
+    availableFns,
+  }
+}
+
+const getAvailableFns = (
+  selectedTool: typeof INTEGRATIONS[number]["value"],
+) => {
+
+  if (selectedTool === SUPPORTED_INTEGRATIONS.JIRA) {
+    const tools = createJiraToolsExport({
+      host: 'test-url',
+      auth: { bearerToken: 'test-token' },
+    }).tools;
+
+    return tools.map((tool) => (
+      '• `' + tool.lc_kwargs.name + '`: ' + tool.lc_kwargs.description
+    ));
+  }
+
+  if (selectedTool === SUPPORTED_INTEGRATIONS.GITHUB) {
+    const tools = createGitHubToolsExport({
+      token: 'test-access-token',
+      owner: 'test-github-owner',
+      repo: 'test-github-repo',
+    }).tools;
+
+    return tools.map((tool) => (
+      '• `' + tool.lc_kwargs.name + '`: ' + tool.lc_kwargs.description
+    ));
+  }
+
+  if (selectedTool === SUPPORTED_INTEGRATIONS.HUBSPOT) {
+    const tools = createHubspotToolsExport({
+      accessToken: 'test-access-token',
+    }).tools;
+
+    return tools.map((tool) => (
+      '• `' + tool.lc_kwargs.name + '`: ' + tool.lc_kwargs.description
+    ));
+  }
+
+  if (selectedTool === SUPPORTED_INTEGRATIONS.POSTGRES) {
+    const tools = createPostgresToolsExport({
+      host: 'test-host',
+      port: 8080,
+      user: 'test-user',
+      password: 'test-password',
+      database: 'test-db',
+      ssl: false,
+    }).tools;
+
+    return tools.map((tool) => (
+      '• `' + tool.lc_kwargs.name + '`: ' + tool.lc_kwargs.description
+    ));
+  }
+
+  if (selectedTool === SUPPORTED_INTEGRATIONS.SALESFORCE) {
+    const tools = createSalesforceToolsExport({
+      instanceUrl: 'test-instance-url',
+      accessToken: 'test-access-token'
+    }).tools;
+
+    return tools.map((tool) => (
+      '• `' + tool.lc_kwargs.name + '`: ' + tool.lc_kwargs.description
+    ));
+  }
+
+  return ['No Available functions.'];
+};
 
 const getToolConnectionView = (selectedTool: typeof INTEGRATIONS[number]['value'] | undefined): BlockBuilder[] => {
   return [
