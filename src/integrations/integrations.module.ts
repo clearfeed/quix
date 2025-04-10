722fe6aa1395531569b03d84950faf2018f57e4e
Amitvikram Dwivedi <56157504+batflarrow@users.noreply.github.com>
2025-04-10 16:11:37 +0530

    feat: add support for connecting to custom MCP server (#149)

--- src/integrations/integrations.module.ts
+++ src/integrations/integrations.module.ts
@@ -3,11 +3,12 @@ import { IntegrationsController } from './integrations.controller';
 import { IntegrationsInstallService } from './integrations-install.service';
 import { HttpModule } from '@nestjs/axios';
 import { IntegrationsService } from './integrations.service';
+import { McpService } from './mcp.service';
 
 @Module({
   imports: [HttpModule],
   controllers: [IntegrationsController],
-  providers: [IntegrationsInstallService, IntegrationsService],
-  exports: [IntegrationsInstallService, IntegrationsService]
+  providers: [IntegrationsInstallService, IntegrationsService, McpService],
+  exports: [IntegrationsInstallService, IntegrationsService, McpService]
 })
 export class IntegrationsModule {}
--- src/llm/llm.module.ts
+++ src/llm/llm.module.ts
@@ -4,11 +4,10 @@ import { LlmProviderService } from './llm.provider';
 import { ConfigModule } from '@nestjs/config';
 import { ToolService } from './tool.service';
 import { IntegrationsModule } from '../integrations/integrations.module';
-import { McpService } from './mcp.service';
 
 @Module({
-  providers: [LlmService, LlmProviderService, ToolService, McpService],
-  exports: [LlmService, McpService],
+  providers: [LlmService, LlmProviderService, ToolService],
+  exports: [LlmService],
   imports: [ConfigModule, IntegrationsModule]
 })
 export class LlmModule {}
